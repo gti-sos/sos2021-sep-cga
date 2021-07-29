87ef24c3ae5b661be5f33143477aaa208a08d81e
cargonare1 <cargonare1@alum.us.es>
2021-07-29 15:11:09 +0200

    prueba

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 var express = require("express");
 
-var bodyParser = require("body-parser");
-
-var psychology_stats_api = require("./psychology-stats");
-
+var app = express();
 
 var port = (process.env.PORT || 10000);
 
 
-var BASE_API_PATH = "/api/v1"; 
-
-var app = express();
-app.use(bodyParser.json());
-app.use(express.json());
-
-psychology_stats_api.register(app, BASE_API_PATH);
-
-var path = require("path");
-
 app.use("/", express.static(path.join(__dirname,"public")));
 
+app.get("/v1", (req, res) => {
+    res.send("<html><body><h1>Buenas GET</h1></body></html>")
+});
+
+app.post("/v1", (req, res) => {
+    res.send("<html><body><h1>Buenas POST</h1></body></html>")
+});
+
 app.listen(port, () => {
-    console.log(`Server ready listening on ${port}`);
+    console.log(`Server ready listening on port ${port}`);
 });
