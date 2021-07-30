8551b72b934fa75a2cb33b909b1152b3d177ef47
cargonare1 <cargonare1@alum.us.es>
2021-07-30 18:39:17 +0200

    prueba

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var express = require("express");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var bodyParser = require("body-parser");
-
-var RM_api = require("./realmadridAPI");
-
-
-var port = (process.env.PORT || 10000);
+app.use(express.json()); // Automatically parses the requests body into a JSON
+app.use(express.static('public')); // Exposes the folder public in the webapp
 
 
-var BASE_API_PATH = "/api/v1"; 
 
-var app = express();
-app.use(bodyParser.json());
-app.use(express.json());
+const realmadridAPI = require("./realmadridAPI/index");
+app.use('/api/v1/realmadridAPI', realmadridAPI);
 
-RM_api.register(app, BASE_API_PATH);
+// ***** RUTAS APIS L07
 
-var path = require("path");
-
-app.use("/", express.static(path.join(__dirname,"public")));
-
-app.listen(port, () => {
-    console.log(`Server ready listening on ${port}`);
+// Carlos
+app.get('/info/realmadridAPI', (req, res) => {
+    res.send("<html><body><h3>realmadridAPI</h3> Estudia los datos del Real Madrid por cada temporada <br></br> <table> <tr><td><b>center &nbsp&nbsp&nbsp&nbsp</b></td> <td><b>year &nbsp&nbsp</b></td> <td><b>fixed-fees &nbsp&nbsp&nbsp&nbsp</b></td> <td><b>amounts-by-number-of-etc &nbsp&nbsp&nbsp&nbsp</b></td> <td><b>amounts-by-number-of-proffesors &nbsp&nbsp&nbsp&nbsp</b></td> <td><b>total &nbsp&nbsp&nbsp&nbsp</b></td></tr>    <tr><td>ETSII</td> <td>2018</td> <td>11.003,22</td> <td>38.717,17</td> <td>93.753,44</td> <td>143.474,00</td></tr>    <tr><td>ETSIA</td> <td>2018</td> <td>11.003,22</td> <td>30.500,00</td> <td>80.46,90</td> <td>120.500,20</td></tr>    <tr><td>FCOM</td> <td>2018</td> <td>11.003,22</td> <td>93.753,44</td> <td>50.700,59</td> <td>180.600,20</td></tr>    <tr><td>ETSI</td> <td>2018</td> <td>11.003,22</td> <td>70.600,45</td> <td>38.717,17</td> <td>143.474,00</td></tr>    <tr><td>FEFP</td> <td>2018</td> <td>11.003,22</td> <td>38.717,17</td> <td>40.090,00</td> <td>143.474,00</td></tr>    <tr><td>FPSYCHOLOGY</td> <td>2018</td> <td>11.003,22</td> <td>80.46,90</td> <td>60.800,43</td> <td>173.474,00</td></tr>    <tr><td>EPS</td> <td>2018</td> <td>11.003,22</td> <td>50.700,59</td> <td>63.435,01</td> <td>112.401,00</td></tr>    <tr><td>FBBAA</td> <td>2018</td> <td>11.003,22</td> <td>40.090,00</td> <td>30.500,00</td> <td>90.474,00</td></tr>    <tr><td>FMEDICINE</td> <td>2018</td> <td>11.003,22</td> <td>40.090,00</td> <td>70.600,45</td> <td>113.474,00</td></tr> </table></body></html>");
 });
+
+// ***** HTMLS DINÁMICOS L06
+
+var server = app.listen(PORT, () =>{
+    console.log(`Listening at http://127.0.0.1:${PORT}`);
+});
+
+module.exports = server;
