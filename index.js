1109c877470196412743b55ef39df32541c22c0b
cargonare1 <cargonare1@alum.us.es>
2021-07-30 10:48:26 +0200

    prueba

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var contactAPI =  require("./medalAPI");
+var medalAPI =  require("./medalAPI");
 
 var PORT = (process.env.PORT || 1607);
 
 var app = express();
 app.use(bodyParser.json());
 
-contactAPI.register(app);
+medalAPI.register(app);
 
 app.listen(PORT,()=>{
     console.log(`Server ready at ${PORT}!`);
