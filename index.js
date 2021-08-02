2183b03847d19c6b84c8c3ae7f6dab366f57d880
cargonare1 <cargonare1@alum.us.es>
2021-08-02 20:22:52 +0200

    vuelta

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var path = require("path");
 
 var bodyParser = require("body-parser");
 
-var olimpic_API =  require("./olimpicAPI");
+var olimpic_APIv1 =  require("./olimpicAPI/v1");
+var olimpic_APIv2 =  require("./olimpicAPI/v2");
 
 
 var PORT = (process.env.PORT || 10000);
@@ -14,7 +15,8 @@ app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname,"public")));
 
 
-olimpic_API.register(app);
+olimpic_APIv1.register(app);
+olimpic_APIv2.register(app);
 
 app.listen(PORT,()=>{
     console.log(`Server ready at ${PORT}!`);
