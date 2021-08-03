096190e11d50d96b43d9023bc153427d9b9d1971
cargonare1 <cargonare1@alum.us.es>
2021-08-04 01:32:46 +0200

    fuera navbar

--- src/frontend/Header.svelte
+++ src/frontend/Header.svelte
@@ -18,8 +18,8 @@
 <Navbar style="background-color:#24355C;" dark
 class="fixed-top">
   <NavbarBrand href="/" class="mr-auto"><strong>sos2021-sep-cga</strong></NavbarBrand>
-  <NavbarToggler on:click={toggleNav} className="mr-2" />
-  <!---<Collapse {isOpen} navbar>
+  <!--<NavbarToggler on:click={toggleNav} className="mr-2" />
+  -<Collapse {isOpen} navbar>
     <Nav navbar>
       <NavItem>
         <NavLink href="#/olimpic-stats">Documentación</NavLink>
