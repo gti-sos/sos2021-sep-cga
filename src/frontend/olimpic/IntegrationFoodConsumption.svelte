b396149835960e3de4bcad555e2a645abe5c5503
cargonare1 <cargonare1@alum.us.es>
2021-08-12 00:13:42 +0200

    integracion 1 con todo correcto creo

--- src/frontend/olimpic/IntegrationFoodConsumption.svelte
+++ src/frontend/olimpic/IntegrationFoodConsumption.svelte
@@ -1,27 +1,27 @@
 <script>
     async function loadChart(){
-        let res1 = await fetch('https://sos2021-10.herokuapp.com/api/v2/foodconsumption-stats');
-        await fetch('https://sos2021-10.herokuapp.com/api/v2/foodconsumption-stats/loadInitialData');
-        let res2 = await fetch("/api/v2/olimpic-stats?city=Barcelona&year=1992")
+        let res1 = await fetch('https://sos2021-24.herokuapp.com/api/v2/children-out-school');
+        await fetch('https://sos2021-24.herokuapp.com/api/v2/children-out-school/loadInitialData');
+        let res2 = await fetch('/api/v2/olimpic-stats?city=Rio&year=2016')
         let res_data1 = await res1.json()
         let res_data2 = await res2.json()
-        let calorias = [];
-        let medallasOro = [];
+        let abandono = [];
+        let olimpic = [];
         let anyos = [];
         let total_label;
         let total_label2;
         res_data1.forEach((data) => {
-                if(data.country == "United_States"){
-                    calorias.push(data.dailycalory);
+                if(data.country == "France"){
+                    abandono.push(data.children_out_school_total);
                     anyos.push(data.year);
-                    total_label = 'Nº calorias al dia en ' + data.country;
+                    total_label = 'Nº abandono en ' + data.country;
                 }
             })
         let anyo = anyos[0];
         res_data2.forEach((data) => {
-                if(data.city == "Pekin" && data.year == anyo){
-                    medallasOro.push(data.gold_medal);
-                    total_label2 = 'Medallas de Oro ganadas ' + data.gold_medal;
+                if(data.city == "Rio" && data.year == anyo){
+                    olimpic.push(data.gold_medal);
+                    total_label2 = 'El número de medallas de oro es de ' + data.gold_medal;
                 }
             })
         var ctx = document.getElementById('myChart').getContext('2d');
@@ -31,11 +31,11 @@
             labels: anyos,
             datasets: [{
                 label: total_label,
-                data: calorias,
+                data: abandono,
                 borderWidth: 1 
             }, {
                 label: total_label2,
-                data: medallasOro,
+                data: olimpic,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -67,7 +67,7 @@
     </script>
     
     <main>
-        <h1>Gráfico que muestra el nº de calorias consumidas en un zona determinada y las medallas ganadas por España en 2008</h1>
+        <h1>Gráfico que muestra el nº de abandono en Sevilla y el olimpic de la matrícula del grado de Ingeniería informática en la US en el año 2017</h1>
         <canvas id="myChart" width="400" height="400"></canvas>
     </main>
     
