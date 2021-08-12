af98be0b7ce9bba0f46f86f751ca87cd970304c3
cargonare1 <cargonare1@alum.us.es>
2021-08-12 03:07:42 +0200

    probando

--- src/frontend/olimpic/IntegrationSmoke.svelte
+++ src/frontend/olimpic/IntegrationSmoke.svelte
@@ -86,7 +86,7 @@ let url = "https://quickchart.io/chart?c="+JSON.stringify(c)
   <br>
   <Button outline color="secondary" onclick="window.location.href='#/olimpic-stats'">Volver</Button>
   <br>
-  <br>-->
+  <br>--->
   <h1>Gráfico que muestra el porcentaje de fumadores y el número de medallas de oro en 2016.</h1>
     <body>
         <img src="" id="graph"/>
