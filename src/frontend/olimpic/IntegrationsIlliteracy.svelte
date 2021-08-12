455dcf8dc603db940615e2b8c9487e6caf7852b6
cargonare1 <cargonare1@alum.us.es>
2021-08-12 11:56:37 +0200

    ahora creo q si

--- src/frontend/olimpic/IntegrationsIlliteracy.svelte
+++ src/frontend/olimpic/IntegrationsIlliteracy.svelte
@@ -1,151 +1,225 @@
 <script>
-  import { CardText, Nav, NavItem, NavLink } from "sveltestrap";
-//import CommonChart from "../../../Pages/CommonChart.svelte";
-  //Uso de API externa Breaking Bad
-  var datos1 = [];
-  var datos2 = [];
-  var inicio = 2008;
-  var errorMsg = "";
-  var okMsg = "";
-  async function getStats() {
-      console.log("Fetching data...");
-      const res = await fetch("/api/v2/olimpic-stats");
-      const res2 = await fetch("https://sos2021-11.herokuapp.com/api/v2/anxiety_stats");
-      if (res.ok && res2.ok) {
-          const json = await res.json();
-          const json2 = await res2.json();
-          datos1 = json;
-          datos2 = json2;
-          //console.log(Object.keys(json));
-          //console.log(Object.keys(datos1));
-          console.log(`We have received ${datos1} datos1.`);
-          console.log(`We have received ${datos2} datos2.`);
-          console.log("Ok");
-      } else {
-          errorMsg = "Error al obtener los  datos1 de los personajes";
-          okMsg = "";
-          console.log("ERROR!" + errorMsg);
-      }
-  }
-  async function onLoad() {
-      await getStats();
-      var NOMBRE1=[];
-      var olimpic=[];
-      var exterior=[];
-      var paisesAPI =[];
-      var ciudadesMIAS =[];
-      var arrayOlimpic=[];
-      var arrayexterior=[];
-     
- 
-      for (let index = 0; index < 10; index++) { 
-        const element = datos1[index]
-        const element2 = datos2[index]
-       // console.log(element);
-        NOMBRE1.push(element.country);
-        ciudadesMIAS.push(element.city);
-              // console.log("NOMBRE1: "+element.country+"_"+element.year);
-        NOMBRE1.push((element2.country).split("_")[1]);
-        paisesAPI.push((element2.country).split("_")[1]);
-        olimpic.push(element.gold_medal);
-        exterior.push(element2.anxiety_men);
-  };
+  import {onMount} from "svelte";
   
-  console.log(NOMBRE1);
-  console.log(olimpic);
-  console.log(exterior);
-  for ( var i = 0; i <10; i++){
-           var decide = NOMBRE1[i] in paisesAPI;
-           console.log(decide);
-           switch( i ){
-             case true:
-             var point1 = { x: "name", y: ""}; // y=number of characters per season
-            point1.x = NOMBRE1[i]
-            point1.y = exterior[i];
-            arrayexterior.push(point1);
-               break;
-              case false:
-              var point2 = { x: "name", y: ""};
-            point2.x = NOMBRE1[i]
-            point2.y = olimpic[i];
-            arrayOlimpic.push(point2);
-            console.log(point2);
-                break;
-           }
-            
-            console.log("quiero ver");
-            console.log(point2);
-            console.log("hola");
-            
-            
-           //console.log(Object.values(point));
-           //console.log(arraymujeres);
-           //array.push(point);
-          }    
-          var chart = JSC.chart('chartDiv', {
-        debug: true,
-        palette: 'fiveColor31',
-        legend_position: 'inside top left',
-        defaultSeries: {
-          type: 'area',
-          shape_opacity: 0.6,
-          defaultPoint_tooltip: '%icon {%percentOfGroup:n1}% <b>%yValue</b>'
-        },
-        yAxis: { scale_type: 'stacked', formatString: 'c' },
-        xAxis: {
-          crosshair_enabled: true,
-          categories: 'Range:'+inicio+'  to 2016'
-        },
-        title_label_text: 'Costs (Last 6 Months)',
-        series: [
-          {
-            name: 'Purchases',
-            points: arrayOlimpic
-          },
-          {
-            name: 'Taxes',
-            points: arrayexterior
-          },
-          
-        ]
-      });
-    
-    console.log("array");
-  }
-</script>
-
-<svelte:head>
-  <script
-  type="text/javascript" src="https://code.jscharting.com/latest/jscharting.js"
-      on:load={onLoad}></script>
+  
+  
+  var BASE_API_PATH = '/api/v2/olimpic-stats';
+  
+  var TOURISM_API_PATH = 'https://sos2021-03.herokuapp.com/api/integration/international-tourisms';
+  
+     
+  
+  //////////////////////////////////////////
+  //Funciones Cargar grafica y datos Api's//
+  //////////////////////////////////////////
+  
+  //Variables comunes
+  
+  var olimpic_data = [];
+  var tourism_data = [];
+  var inicio = 2014;
+  var fin = 2017;
+  var anyos = rangoAnyos(inicio,fin);
+  
+  
+  async function tomaDatosGrafica(datos,atributo){ //Esta funcion hace la media por años y 
       
-</svelte:head>
-
-<main>
-  <Nav>
-      <NavItem>
-          <NavLink href="/">Página Principal</NavLink>
-      </NavItem>
-      <NavItem>
-          <NavLink href="/#/integrations/">volver</NavLink>
-      </NavItem>
-  </Nav>
-
-  <div>
-      <h2>Uso API Propia </h2>
-      <CardText> Representación de la tasa de alfabetización por pais y año usando la biblioteca jscharting</CardText>
-  </div>
-
-  {#if errorMsg}
-      <p>{errorMsg}</p>
-  {:else}
-      <div id="chartDiv"  />
-  {/if}
-</main>
-
-<style>
-  #chartDiv {
-      width: 100%;
-      height: 400px;
+  
+      //Filtramos para usar datos seleccionados a partir de año de inicio
+      var datosFiltradosAnyo = datos.filter((e)=>{
+          return e.year >= inicio;
+      });
+  
+      console.log("Datos filtrados:" + datosFiltradosAnyo);
+  
+      //Creamos variables auxiliares
+      var arrayTotal = [];
+      
+      var anyos = rangoAnyos(inicio,fin);
+      var a = 0;
+  
+      var mediaPorAnyo = 0;
+      var arrayFinal = [];
+      var contador0 = 0;
+      var contadorDist = 0;
+  
+      //Iteramos por cada año del rango establecido
+      for(var anyo in anyos){
+          //Pillamos el año
+          a=anyos[anyo];
+          //Limpiamos variables
+          mediaPorAnyo = 0;
+          arrayTotal=[];
+          
+          //Iteramos sobre los datos para comprobar si su año coincide con el establecido
+          for(var num in datosFiltradosAnyo){
+              var dato = datosFiltradosAnyo[num]; //Tomamos el dato que estamos iterando
+              if(dato.year == a){ //Si coincide con el año ("a") se toma el valor del atributo pasado por parametro
+                  arrayTotal.push(dato[atributo]);
+              }
+              else{
+                  arrayTotal.push(0);
+              }
+         }
+  
+         console.log("Total olimpic " + num +" " + arrayTotal);
+         
+         //Hacemos la media por años
+  
+         for(var i = 0; i < arrayTotal.length; ++i){
+          if(arrayTotal[i] == 0)
+              contador0++;
+          else
+              contadorDist++;
+          }
+  
+  
+         for(var num in arrayTotal){
+             mediaPorAnyo += arrayTotal[num];
+         }
+  
+         if(contador0 == anyos.length){
+             mediaPorAnyo = 0;
+         }
+         else{
+             mediaPorAnyo = mediaPorAnyo / contadorDist;
+         }
+  
+         mediaPorAnyo = Math.round(mediaPorAnyo);
+         
+         var objeto =  { x: a, y: mediaPorAnyo }
+  
+         //Pusheamos al array final
+         arrayFinal.push(objeto);
+      }
+  
+      return arrayFinal;
   }
-</style>
+  
+  
+  
+  async function cargaGrafica(){
+  
+      const res_ee = await fetch(BASE_API_PATH);
+      const res_r = await fetch(TOURISM_API_PATH);
+  
+      if (res_ee.ok){
+          var json_ee = await res_ee.json();
+          
+          if(json_ee.length===undefined){
+          olimpic_data = [];
+          olimpic_data.push(json_ee);          
+          }
+          else{
+          olimpic_data = json_ee;
+          }
+  
+      }
+      if(res_r.ok){
+          var json_r = await res_r.json();
+  
+          if(json_r.length===undefined){
+              console.log("Aqui llega undefined javi");
+          tourism_data = [];
+          tourism_data.push(json_r);          
+          }
+          else{
+          console.log("Aqui llegan datos javi");
+          console.log("longitud:" + json_r.length);
+          tourism_data = json_r;
+          console.log(json_r);
+          }
+  
+      }
+  
+      
+      /*tomamos los años y el dato a buscar de los elementos seleccionados
+      for(var elemento in olimpic_data){
+          console.log(elemento);
+          anyos.push(olimpic_data[elemento].year);
+          data_clasif.push(olimpic_data[elemento][datoClasif]);
+      }
+      console.log("años: " + anyos);
+      console.log("datos " + datoClasifEsp + ":" + data_clasif);
+      conjuntoAnyos = new Set(anyos);
+      anyos = [...conjuntoAnyos];*/
+  
+      //Tomamos los datos
+  
+      var datosGrafica_edex = await tomaDatosGrafica(olimpic_data,"education_expenditure_per_millions");
+      console.log("edex final:" + datosGrafica_edex);
+      var datosGrafica_tourism    = await tomaDatosGrafica(tourism_data,"expendituresbillion");
+      console.log("tourism final:" + datosGrafica_tourism);
+  
+      
+      var anyosGraphic = [];
+      var aux = "";
+  
+      for(var a in anyos){
+          aux = String(anyos[a]);
+          anyosGraphic.push(aux);
+      }
+      console.log("años grafica:" + anyosGraphic);
+      var obj_edex = {
+  type: "splineArea",
+  showInLegend: true,
+  name: "Gastos en educación (millones de euros)",
+  yValueFormatString: "€",
+  xValueFormatString: "",
+  dataPoints: datosGrafica_edex
+     };
+       var obj_tourism = {
+  type: "splineArea",
+  showInLegend: true,
+  name: "Gastos en turismo (billones de euros)",
+  yValueFormatString: "€",
+  xValueFormatString: "",
+  dataPoints: datosGrafica_tourism
+ }
+  
+   var datos_grafica = [];
+   datos_grafica.push(obj_edex);
+   datos_grafica.push(obj_tourism);
+      var chart = new CanvasJS.Chart("chartContainer", {
+          animationEnabled: true,
+          title:{
+              text: "Gatos en educación vs Gastos en Turismo"
+          },
+          axisY :{
+              includeZero: false
+              
+          },
+          toolTip: {
+              shared: true
+          },
+          legend: {
+              fontSize: 13
+          },
+          data: datos_grafica
+      });
+      chart.render();
+};
+  // Funciones auxiliares
+  
+  function rangoAnyos(inic,fin){
+      var rango = [];
+      for(var i = inic; i<=fin;i++){
+          rango.push(i);
+      }
+      return rango;
+  }
+  
+  </script>
+  
+  <svelte:head>
+      <!--Se hace uso de la biblioteca CanvasJS y el tipo es  Multi Series Spline Area-->    
+      <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js" on:load="{cargaGrafica}"></script>
+  </svelte:head>
+  
+  <main>
+      <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+  </main>
+  
+  <style>
+  
+  </style>
