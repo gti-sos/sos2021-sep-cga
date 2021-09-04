ed75df119ab9015eedb849b064b657ac39e474ba
cargonare1 <cargonare1@alum.us.es>
2021-09-04 11:19:03 +0200

    Video

--- test/e2e.js
+++ test/e2e.js
@@ -10,11 +10,10 @@ if (!fs.existsSync("e2e_capturas")){
   const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
-   // await page.setViewport({ width: 1366, height: 768});
+    await page.setViewport({ width: 1366, height: 768});
 
     console.log("-- Test 1 - Cargar la pagina principal");
-    //await page.goto('http://localhost:10000/');
-    await page.goto('http://google.com/');
+    await page.goto('https://sos2021-sep-cga.herokuapp.com/');
     await page.screenshot({ path: url + 'PaginaPrincipal.png' });
 
     console.log("-- Test 1 - Acabado\n-----------------");
